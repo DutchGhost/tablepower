9cb82021df1216b23135f30287a2e10d86e1f571
Dodo <kasper199914@gmail.com>
2019-04-24 12:37:17 +0200

    add order specifier, and exponent specifier

--- examples/power.rs
+++ examples/power.rs
@@ -1,10 +1,10 @@
 #[macro_use]
 extern crate tablepower;
 
-table_of!(u8, table);
+table_of!(u8, table, order = descending);
 
 fn main() {
     assert!(table.len() == 3);
-
+    assert_eq!(table, [100, 10, 1]);
     println!("{:?}", table);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,18 @@ use self::syn::{
     parse_macro_input, Result, Token, Type,
 };
 
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
+enum Order {
+    Ascending,
+    Descending,
+}
+
 #[derive(Debug)]
 struct Pow10 {
     integer_type: syn::Type,
     name: syn::Ident,
+    order: Order,
+    exponent: usize,
 }
 
 impl Parse for Pow10 {
@@ -26,7 +34,53 @@ impl Parse for Pow10 {
 
         let name: syn::Ident = input.parse()?;
 
-        Ok(Self { integer_type, name })
+        let _: Option<Token![,]> = input.parse()?;
+
+        let order: Option<syn::Ident> = input.parse()?;
+
+        let order_of_table = order.map(|order| {
+            if order != "order" {
+                return Err(syn::Error::new(order.span(), "Expected `order`"));
+            }
+
+            let _: Token![=] = input.parse()?;
+
+            let order_argument: syn::Ident = input.parse()?;
+
+            if order_argument == "ascending" {
+                Ok(Order::Ascending)
+            } else if order_argument == "descending" {
+                Ok(Order::Descending)
+            } else {
+                Err(syn::Error::new(order.span(), "Invalid order specifier specified!. Did you mean `ascending` or `descending`?"))
+            }
+ 
+        }).unwrap_or(Ok(Order::Ascending))?;
+
+        let _: Option<Token![,]> = input.parse()?;
+
+        let exponent: Option<syn::Ident> = input.parse()?;
+
+        let exponent_value = exponent
+            .map(|exponent| {
+                if exponent != "exponent" {
+                    return Err(syn::Error::new(exponent.span(), "Expected `exponent`"));
+                }
+
+                let _: Token![=] = input.parse()?;
+
+                let value: syn::LitInt = input.parse()?;
+
+                Ok(value.value())
+            })
+            .unwrap_or(Ok(10))?;
+
+        Ok(Self {
+            integer_type,
+            name,
+            order: order_of_table,
+            exponent: exponent_value as usize,
+        })
     }
 }
 
@@ -42,39 +96,60 @@ impl Pow10 {
     }
 }
 
-// @TODO: Replace this with pow10 macro.
-trait Integer {
-    const LEN: usize;
-}
+macro_rules! powers {
+    ($int:ty, exponent = $exponent:expr) => {{
+        struct Powers {
+            current: $int,
+            exponent: $int,
+            done: bool,
+        }
 
-impl Integer for usize {
-    const LEN: usize = 20;
-}
+        impl Powers {
+            fn new(exponent: usize) -> Self {
+                Self {
+                    current: 1,
+                    exponent: exponent as $int,
+                    done: false,
+                }
+            }
+        }
 
-impl Integer for u64 {
-    const LEN: usize = 20;
-}
+        impl Iterator for Powers {
+            type Item = $int;
 
-impl Integer for u32 {
-    const LEN: usize = 10;
-}
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.done {
+                    return None;
+                }
 
-impl Integer for u16 {
-    const LEN: usize = 5;
-}
+                let ret = self.current;
 
-impl Integer for u8 {
-    const LEN: usize = 3;
+                let (new, overflowed) = self.current.overflowing_mul(self.exponent);
+
+                if overflowed {
+                    self.done = true;
+                } else {
+                    self.current = new;
+                }
+                Some(ret)
+            }
+        }
+
+        Powers::new($exponent)
+    }};
 }
 
 macro_rules! build_array {
-    ($int_type:ty, $name:expr) => {{
+    ($int_type:ty, $name:expr, order = $order:expr, exponent = $exponent:expr) => {{
 
-        let len = <$int_type>::LEN;
+        let mut v = powers!($int_type, exponent = $exponent).map(|n| n as $int_type).collect::<Vec<_>>();
 
-        let v = Powers::new().take(len).map(|n| n as $int_type).collect::<Vec<_>>();
+        if $order == Order::Descending {
+            v.reverse();
+        }
 
         let name = $name;
+        let len = v.len();
 
         quote!(const #name: [$int_type; #len] = [#(#v),*];).into()
     }}
@@ -86,12 +161,15 @@ pub fn table_of(tokens: TokenStream) -> TokenStream {
 
     let name = input.name.clone();
 
+    let order = input.order;
+    let exponent = input.exponent;
+
     match &*input.yank_int_type().unwrap() {
-        "usize" => build_array!(usize, name),
-        "u64" => build_array!(u64, name),
-        "u32" => build_array!(u32, name),
-        "u16" => build_array!(u16, name),
-        "u8" => build_array!(u8, name),
+        "usize" => build_array!(usize, name, order = order, exponent = exponent),
+        "u64" => build_array!(u64, name, order = order, exponent = exponent),
+        "u32" => build_array!(u32, name, order = order, exponent = exponent),
+        "u16" => build_array!(u16, name, order = order, exponent = exponent),
+        "u8" => build_array!(u8, name, order = order, exponent = exponent),
 
         ty => {
             let s = format!("table_of not implemented for type `{}`", ty);
@@ -99,40 +177,5 @@ pub fn table_of(tokens: TokenStream) -> TokenStream {
                 .to_compile_error()
                 .into()
         }
-    }
-}
-
-struct Powers {
-    current: usize,
-    done: bool,
-}
-
-impl Powers {
-    fn new() -> Self {
-        Self {
-            current: 1,
-            done: false,
-        }
-    }
-}
-
-impl Iterator for Powers {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.done {
-            return None;
-        }
-
-        let ret = self.current;
-
-        let (new, overflowed) = self.current.overflowing_mul(10);
-
-        if overflowed {
-            self.done = true;
-        } else {
-            self.current = new;
-        }
-        Some(ret)
     }
 }
